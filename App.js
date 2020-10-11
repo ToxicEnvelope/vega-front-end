931cfee6b6f9042d00b26135ca0da79b31f969a1
ToxicEnvelope <ynhoffman@gmail.com>
2020-10-12 00:31:57 +0300

    update main app view

--- App.js
+++ App.js
@@ -1,21 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { SecureStore } from "expo";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import LoginViewComponent from "./components/LoginView/loginViewComponent";
+import NoTokenView from "./components/NoTokenView/NoTokenViewComponent";
+import HomeView from "./components/homeView/homeViewComponent";
+
+const AppView = (props) => {
+  const [hasToken, setToken] = useState(true);
+
+  useState(() => {
+    // get token from store
+    const token = null;
+    setToken(token);
+    console.log("Token: " + token);
+  });
+
+  switch (hasToken) {
+    case true:
+      return (
+        <>
+          <HomeView></HomeView>
+        </>
+      );
+    case false:
+      return (
+        <>
+          <LoginViewComponent></LoginViewComponent>
+        </>
+      );
+    default:
+      return (
+        <>
+          <NoTokenView></NoTokenView>
+        </>
+      );
+  }
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <AppView></AppView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  app: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
